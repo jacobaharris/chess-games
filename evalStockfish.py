f8259407516fc97f58c369e573bdbb7b02043a98
Jacob Harris <31747373+jacobaharris@users.noreply.github.com>
2022-09-05 09:11:47 -0500

    create generateTestFEN, sampleFEN

--- evalStockfish.py
+++ evalStockfish.py
@@ -16,19 +16,5 @@ def main():
     stock3 = Stockfish(path='/usr/local/Cellar/stockfish/15/bin/stockfish",
                        depth=inDepth,
                        parameters={"Hash":512})
-    #create list of fens to sample from
-    fens = open('/Users/admin/Desktop/Lichess/FENeval.txt','r')
-    fenList = []
-    for line in fens:
-        fenList.append(line.split(':')[0])
-    fens.close()
-    randList = []
-    while len(randList)<10:
-        a = random.randint(0,len(fensList))
-        if a in randList:
-            continue
-        else:
-            randList.append(a)
     
-        
 main()
