b49cc819b7ee811569d0ce83a337d6cf4a11bd40
Jacob Harris <31747373+jacobaharris@users.noreply.github.com>
2022-09-27 17:42:05 -0500

    Update CreateDatabase.py

--- CreateDatabase.py
+++ CreateDatabase.py
@@ -3,7 +3,7 @@ from lichess.format import PGN
 import json
 
 def getToken():
-    file = open('/Users/admin/Desktop/Lichess/Token.txt','r')
+    file = open('/Users/admin/Desktop/chess-games/Token.txt','r')
     token = file.read()
     return str(token)
 
@@ -12,7 +12,7 @@ def main():
     mygames = list(lichess.api.user_games('jacobaharris',auth=token,rated=True,
                                      format=PGN))
     mygames.reverse()
-    with open('/Users/admin/Desktop/Lichess/data.pgn','w') as f:
+    with open('/Users/admin/Desktop/chess-games/data.pgn','w') as f:
         for i in mygames:
             print(i, file = f)
 main()
